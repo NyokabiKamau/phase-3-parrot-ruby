6848f712800beca656f563aa341913903dbd6ca4
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-02-10 01:57:14 +0300

    Done with methods and return values assignment

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot(phrase = "Squawk!")
+    if phrase == ''
+        puts phrase
+    else
+        puts phrase
+    end
+    phrase
+end
+puts parrot
